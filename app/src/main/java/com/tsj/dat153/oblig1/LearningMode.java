11242aa1f0a0f4fdf334f38b15ed392a005ffabf
sinstok <sindre.stokkenes@gmail.com>
2017-01-12 17:24:46 +0100

    lagde læringsmodus xml-filen

--- app/src/main/java/com/tsj/dat153/oblig1/LearningMode.java
+++ app/src/main/java/com/tsj/dat153/oblig1/LearningMode.java
@@ -2,6 +2,10 @@ package com.tsj.dat153.oblig1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
 
 public class LearningMode extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class LearningMode extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning_mode);
+        int counter = 0;
+        int score = 0;
+
+        List<Person> liste = MainActivity.liste;
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_learning_mode);
+        ImageView image = new ImageView(this);
+        image.setImageResource(R.drawable.lionel_richie);
+        layout.addView(image, 0);
     }
 }
