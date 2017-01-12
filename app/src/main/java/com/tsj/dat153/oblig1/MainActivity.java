53c9a66279e8722902b40b5f34c1bf973a84d0a4
Joakim <joakimtran@gmail.com>
2017-01-12 12:51:25 +0100

    la til knapper i main

--- app/src/main/java/com/tsj/dat153/oblig1/MainActivity.java
+++ app/src/main/java/com/tsj/dat153/oblig1/MainActivity.java
@@ -1,7 +1,9 @@
 package com.tsj.dat153.oblig1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toNameList(View view){
+        Intent intent = new Intent(this, NameList.class);
+        startActivity(intent);
+    }
+
+    public void toImageGallery(View view){
+        Intent intent = new Intent(this, ImageGallery.class);
+        startActivity(intent);
+    }
+
+    public void toLearningMode(View view){
+        Intent intent = new Intent(this, LearningMode.class);
+        startActivity(intent);
     }
 }
