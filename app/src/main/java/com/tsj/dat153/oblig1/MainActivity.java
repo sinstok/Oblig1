c4938591606835fe58880083c89432a8ffcb06d7
sinstok <sindre.stokkenes@gmail.com>
2017-01-12 13:58:43 +0100

    noe

--- app/src/main/java/com/tsj/dat153/oblig1/MainActivity.java
+++ app/src/main/java/com/tsj/dat153/oblig1/MainActivity.java
@@ -1,9 +1,12 @@
 package com.tsj.dat153.oblig1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected  void onStart(){
+        super.onStart();
+        PersonContainer container = (PersonContainer) getApplicationContext();
+        Person p1 = new Person("Jerry", R.drawable.seinfeld_jerry);
+        Person p2 = new Person("George", R.drawable.george_costanza);
+        Person p3 = new Person("Kramer", R.drawable.kramer);
+        container.addPerson(p1);
+        container.addPerson(p2);
+        container.addPerson(p3);
     }
 
     public void toNameList(View view){
--- app/src/main/java/com/tsj/dat153/oblig1/NameList.java
+++ app/src/main/java/com/tsj/dat153/oblig1/NameList.java
@@ -3,28 +3,46 @@ package com.tsj.dat153.oblig1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.List;
 
 
 public class NameList extends AppCompatActivity {
-    private Person p1;
-    private Person p2;
-    private Person p3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_list);
+    }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+        PersonContainer cont = (PersonContainer) getApplicationContext();
+        List<Person> liste = cont.getPersonList();
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_name_list);
+        /*Button b1 = new Button(this);
+        b1.setText();
+        Button*/
+        for(int i = 0; i < liste.size(); i++){
+            Button button = new Button(this);
+            button.setText(liste.get(i).getName());
+            layout.addView(button);
+        }
 
     }
 
     public void viewImage(View view){
-        p1 = new Person("Sindre", R.drawable.seinfeld_jerry);
-        //p2 = new Person("Joakim", "android.resources://" + "com.tsj.dat153.oblig1/drawable/George-costanze.jpg");
-        //p3 = new Person("Tomas", "android.resources://" + "com.tsj.dat153.oblig1/drawable/Kramer.jpg");
-        ImageView layout = (ImageView) findViewById(R.id.image);
-        layout.setImageResource(p1.getImage());
+
+        /*ImageView image = (ImageView) findViewById(R.id.image);
+        image.setImageResource(p1.getImage());*/
+        /*TextView textView = new TextView(this);
+        textView.setText(p1.getName());
+        layout.addView(textView);*/
     }
 }
