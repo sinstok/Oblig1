8ce3a16a2863f225ec50c6dc18c6126d36b7bc14
sinstok <sindre.stokkenes@gmail.com>
2017-01-17 15:00:17 +0100

    La til tilfeldig rekkefølge av bilder i en liste. Går tilbake til mainactivity når du er ferdig

--- app/src/main/java/com/tsj/dat153/Activities/LearningModeActivity.java
+++ app/src/main/java/com/tsj/dat153/Activities/LearningModeActivity.java
@@ -1,5 +1,6 @@
 package com.tsj.dat153.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -13,6 +14,7 @@ import android.widget.TextView;
 import com.tsj.dat153.database.DAO;
 import com.tsj.dat153.oblig1.R;
 
+import java.util.Collections;
 import java.util.Random;
 
 import java.util.List;
@@ -26,7 +28,8 @@ public class LearningModeActivity extends AppCompatActivity {
     private int counter;
     private int score;
     private Random number;
-    private ViewGroup layout = (ViewGroup) findViewById(R.id.activity_learning_mode);
+    private ViewGroup layout;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,15 +38,24 @@ public class LearningModeActivity extends AppCompatActivity {
         counter = 0;
         score = 0;
 
+        image = (ImageView) findViewById(R.id.image);
+
         ActionBar bar = getSupportActionBar();
         bar.setDisplayHomeAsUpEnabled(true);
 
         liste = DAO.getPersonList();
 
         number = new Random();
-        randNumb = number.nextInt(liste.size() - 1);
+        //randNumb = number.nextInt(liste.size() - 1);
 
-        Person pers = liste.get(randNumb);
+        long seed = System.nanoTime();
+        Collections.shuffle(liste, new Random(seed));
+
+        layout = (ViewGroup) findViewById(R.id.activity_learning_mode);
+
+        //Person pers = liste.get(counter);
+
+        nextPerson();
 
         /*ViewGroup layout = (ViewGroup) findViewById(R.id.activity_learning_mode);
         ImageView image = new ImageView(this);
@@ -58,7 +70,7 @@ public class LearningModeActivity extends AppCompatActivity {
         EditText textView = (EditText) findViewById(R.id.guess_name);
         Editable text = textView.getEditableText();
         String name = text.toString();
-        Person per = liste.get(randNumb);
+        Person per = liste.get(counter);
 
         counter++;
         String count = Integer.toString(counter);
@@ -69,14 +81,18 @@ public class LearningModeActivity extends AppCompatActivity {
             String sco = Integer.toString(score);
             scoreView.setText(sco);
         }
-        randNumb = number.nextInt(liste.size() - 1);
+        textView.setText("");
+        nextPerson();
     }
 
-    public void changePerson(Person pers){
-        //ViewGroup layout = (ViewGroup) findViewById(R.id.activity_learning_mode);
-        ImageView image = new ImageView(this);
-        image.setImageResource(pers.getImage());
-        image.setId(randNumb);
-        layout.addView(image, 0);
+    public void nextPerson(){
+        if(counter < liste.size()){
+            Person pers = liste.get(counter);
+            image.setImageResource(pers.getImage());
+        } else {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
