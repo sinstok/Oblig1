44e4d2f4fa41652d836b257cec4f7bff40e0d854
sinstok <sindre.stokkenes@gmail.com>
2017-01-17 14:40:11 +0100

    la til noe i learningmode

--- app/src/main/java/com/tsj/dat153/Activities/LearningModeActivity.java
+++ app/src/main/java/com/tsj/dat153/Activities/LearningModeActivity.java
@@ -2,9 +2,13 @@ package com.tsj.dat153.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.support.v7.app.ActionBar;
+import android.widget.TextView;
 
 import com.tsj.dat153.database.DAO;
 import com.tsj.dat153.oblig1.R;
@@ -17,26 +21,62 @@ import com.tsj.dat153.model.Person;
 
 public class LearningModeActivity extends AppCompatActivity {
 
+    private int randNumb;
+    private List<Person> liste;
+    private int counter;
+    private int score;
+    private Random number;
+    private ViewGroup layout = (ViewGroup) findViewById(R.id.activity_learning_mode);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning_mode);
-        int counter = 0;
-        int score = 0;
+        counter = 0;
+        score = 0;
 
         ActionBar bar = getSupportActionBar();
         bar.setDisplayHomeAsUpEnabled(true);
 
-        List<Person> liste = DAO.getPersonList();
+        liste = DAO.getPersonList();
 
-        Random number = new Random();
-        int randNumb = number.nextInt(liste.size() - 1);
+        number = new Random();
+        randNumb = number.nextInt(liste.size() - 1);
 
         Person pers = liste.get(randNumb);
 
-        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_learning_mode);
+        /*ViewGroup layout = (ViewGroup) findViewById(R.id.activity_learning_mode);
         ImageView image = new ImageView(this);
         image.setImageResource(pers.getImage());
+        image.setId(randNumb);
+        layout.addView(image, 0);*/
+    }
+
+    public void onGuess(View v){
+        TextView counterView = (TextView) findViewById(R.id.counter);
+        TextView scoreView = (TextView) findViewById(R.id.score);
+        EditText textView = (EditText) findViewById(R.id.guess_name);
+        Editable text = textView.getEditableText();
+        String name = text.toString();
+        Person per = liste.get(randNumb);
+
+        counter++;
+        String count = Integer.toString(counter);
+        counterView.setText(count);
+
+        if(per.getName().equals(name)){
+            score++;
+            String sco = Integer.toString(score);
+            scoreView.setText(sco);
+        }
+        randNumb = number.nextInt(liste.size() - 1);
+    }
+
+    public void changePerson(Person pers){
+        //ViewGroup layout = (ViewGroup) findViewById(R.id.activity_learning_mode);
+        ImageView image = new ImageView(this);
+        image.setImageResource(pers.getImage());
+        image.setId(randNumb);
         layout.addView(image, 0);
     }
 }
